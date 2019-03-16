0cdb97044ef38350a19d0697aa2a0157cf06bc26
Peter Doornbosch <peter.doornbosch@luminis.eu>
2019-03-16 15:43:08 +0100

    parse new session ticket message

--- src/net/luminis/tls/NewSessionTicketMessage.java
+++ src/net/luminis/tls/NewSessionTicketMessage.java
@@ -4,16 +4,48 @@ import java.nio.ByteBuffer;
 
 public class NewSessionTicketMessage extends HandshakeMessage {
 
-    public NewSessionTicketMessage parse(ByteBuffer buffer, int length, TlsState state) {
-        for (int i = 0; i < length; i++)
-            buffer.get();
-        Logger.debug("Got New Session Ticket message (" + length + " bytes)");
+    private long ticketAgeAdd;
+    private byte[] ticket;
+    private byte[] ticketNonce;
 
+    private long ticketLifetime;
+
+    public NewSessionTicketMessage parse(ByteBuffer buffer, int length, TlsState state) throws TlsProtocolException {
+        buffer.getInt();  // Skip message type and 3 bytes length
+
+        ticketLifetime = buffer.getInt() & 0xffffffffL;
+        ticketAgeAdd = buffer.getInt() & 0xffffffffL;
+        int ticketNonceSize = buffer.get() & 0xff;
+        ticketNonce = new byte[ticketNonceSize];
+        buffer.get(ticketNonce);
+        int ticketSize = buffer.getShort() & 0xffff;
+        ticket = new byte[ticketSize];
+        buffer.get(ticket);
+
+        EncryptedExtensions.parseExtensions(buffer);
+
+        Logger.debug("Got New Session Ticket message (" + length + " bytes)");
         return this;
     }
 
     @Override
     public byte[] getBytes() {
         return new byte[0];
+    }
+
+    public long getTicketLifetime() {
+        return ticketLifetime;
+    }
+
+    public long getTicketAgeAdd() {
+        return ticketAgeAdd;
+    }
+
+    public byte[] getTicket() {
+        return ticket;
+    }
+
+    public byte[] getTicketNonce() {
+        return ticketNonce;
     }
 }
