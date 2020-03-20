113907589eb50cb3f7a2169f0982bfb1eda44551
Peter Doornbosch <peter.doornbosch@luminis.eu>
2020-03-20 22:22:28 +0100

    parse server name extension

--- src/net/luminis/tls/extension/ServerNameExtension.java
+++ src/net/luminis/tls/extension/ServerNameExtension.java
@@ -1,16 +1,51 @@
 package net.luminis.tls.extension;
 
+import net.luminis.tls.DecodeErrorException;
 import net.luminis.tls.TlsConstants;
 
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 
+/**
+ * TLS server name extension: RFC 6066
+ * https://tools.ietf.org/html/rfc6066#section-3
+ */
 public class ServerNameExtension extends Extension {
 
     private final String serverName;
 
     public ServerNameExtension(String serverName) {
         this.serverName = serverName;
+    }
+
+    public ServerNameExtension(ByteBuffer buffer) throws DecodeErrorException {
+        int extensionDataLength = parseExtensionHeader(buffer, TlsConstants.ExtensionType.server_name);
+
+        int serverNameListLength = buffer.getShort();
+        if (extensionDataLength != serverNameListLength + 2) {
+            throw new DecodeErrorException("inconsistent length");
+        }
+
+        int startPosition = buffer.position();
+        serverName = parseServerName(buffer);
+        if (buffer.position() - startPosition != serverNameListLength) {
+            throw new DecodeErrorException("inconsistent length");
+        }
+    }
+
+    private String parseServerName(ByteBuffer buffer) {
+        int nameType = buffer.get();
+        switch (nameType) {
+            case 0:
+                // host_name
+                int hostNameLength = buffer.getShort();
+                byte[] hostNameBytes = new byte[hostNameLength];
+                buffer.get(hostNameBytes);
+                // "The hostname is represented as a byte string using ASCII encoding without a trailing dot. "
+                return new String(hostNameBytes, Charset.forName("ASCII"));
+        }
+        // unsupported type, RFC 6066 only defines hostname
+        throw new RuntimeException();
     }
 
     @Override
@@ -26,8 +61,12 @@ public class ServerNameExtension extends Extension {
         buffer.putShort((short) (hostnameLength + 1 + 2));  // Length of server_name_list
         buffer.put((byte) 0x00);  // list entry is type 0x00 "DNS hostname"
         buffer.putShort(hostnameLength);  // Length of hostname
-        buffer.put(serverName.getBytes(Charset.forName("ISO-8859-1")));
+        buffer.put(serverName.getBytes(Charset.forName("ASCII")));
 
         return buffer.array();
+    }
+
+    public String getHostName() {
+        return serverName;
     }
 }
---
+++ test/net/luminis/tls/extension/ServerNameExtensionTest.java
@@ -0,0 +1,70 @@
+package net.luminis.tls.extension;
+
+import net.luminis.tls.ByteUtils;
+import net.luminis.tls.DecodeErrorException;
+import org.junit.jupiter.api.Test;
+
+import java.nio.ByteBuffer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServerNameExtensionTest {
+
+    @Test
+    void parseServerNameExtension() throws Exception {
+        ByteBuffer buffer = ByteBuffer.wrap(ByteUtils.hexToBytes("0000" + "000e" + "000c" + "00" + "0009" + "6c6f63616c686f7374"));
+
+        ServerNameExtension serverNameExtension = new ServerNameExtension(buffer);
+
+        assertThat(serverNameExtension.getHostName()).isEqualToIgnoringCase("localhost");
+    }
+
+    @Test
+    void serializeServerNameExtension() throws Exception {
+        byte[] serializedData = new ServerNameExtension("localhost").getBytes();
+
+        ServerNameExtension serverNameExtension = new ServerNameExtension(ByteBuffer.wrap(serializedData));
+
+        assertThat(serverNameExtension.getHostName()).isEqualToIgnoringCase("localhost");
+        assertThat(serializedData).startsWith(0x00, 0x00, 0x00, 0x0e, 0x00, 0x0c, 0x00, 0x00, 0x09);
+    }
+
+    @Test
+    void parseUnderflow1() throws Exception {
+        ByteBuffer buffer = ByteBuffer.wrap(ByteUtils.hexToBytes("0000" + "000e" + "000c" + "00" + "0009"));
+
+        assertThatThrownBy(() ->
+                new ServerNameExtension(buffer)
+        ).isInstanceOf(DecodeErrorException.class);
+    }
+
+    @Test
+    void parseUnderflow2() throws Exception {
+        ByteBuffer buffer = ByteBuffer.wrap(ByteUtils.hexToBytes("0000" + "000e" + "000c" + "00" + "0009" + "6c6f63616c686f73"));
+
+        assertThatThrownBy(() ->
+                new ServerNameExtension(buffer)
+        ).isInstanceOf(DecodeErrorException.class);
+    }
+
+    @Test
+    void parseInconsistentLength1() throws Exception {
+        ByteBuffer buffer = ByteBuffer.wrap(ByteUtils.hexToBytes("0000" + "000e" + "000b" + "00" + "0009" + "6c6f63616c686f7374"));
+
+        assertThatThrownBy(() ->
+                new ServerNameExtension(buffer)
+        ).isInstanceOf(DecodeErrorException.class);
+    }
+
+    @Test
+    void parseInconsistentLength2() throws Exception {
+        ByteBuffer buffer = ByteBuffer.wrap(ByteUtils.hexToBytes("0000" + "000e" + "000c" + "00" + "0007" + "6c6f63616c686f7374"));
+
+        assertThatThrownBy(() ->
+                new ServerNameExtension(buffer)
+        ).isInstanceOf(DecodeErrorException.class);
+    }
+
+}
