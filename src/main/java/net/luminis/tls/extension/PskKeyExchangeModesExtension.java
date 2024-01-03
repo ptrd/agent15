f5adaf80bcd3326553e8cb6f94dd43afa6ad8688
Peter Doornbosch <peter.doornbosch@luminis.eu>
2024-01-03 23:06:49 +0100

    when parsing psk key exchange modes extension, ignore unknown code points

--- src/main/java/net/luminis/tls/extension/PskKeyExchangeModesExtension.java
+++ src/main/java/net/luminis/tls/extension/PskKeyExchangeModesExtension.java
@@ -18,12 +18,14 @@
  */
 package net.luminis.tls.extension;
 
-import net.luminis.tls.alert.DecodeErrorException;
 import net.luminis.tls.TlsConstants;
+import net.luminis.tls.alert.DecodeErrorException;
 
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * TLS Pre-Shared Key Exchange Modes extension.
@@ -51,16 +53,14 @@ public class PskKeyExchangeModesExtension extends Extension {
         }
         for (int i = 0; i < pskKeyExchangeModesLength; i++) {
             int modeByte = buffer.get();
-            if (modeByte == TlsConstants.PskKeyExchangeMode.psk_ke.value) {
-                keyExchangeModes.add(TlsConstants.PskKeyExchangeMode.psk_ke);
-            }
-            else if (modeByte == TlsConstants.PskKeyExchangeMode.psk_dhe_ke.value) {
-                keyExchangeModes.add(TlsConstants.PskKeyExchangeMode.psk_dhe_ke);
-            }
-            else {
-                throw new DecodeErrorException("invalid psk key exchange mocde");
-            }
+            decode(modeByte).ifPresent(m -> keyExchangeModes.add(m));
         }
+    }
+
+    static Optional<TlsConstants.PskKeyExchangeMode> decode(int mode) {
+        return Arrays.stream(TlsConstants.PskKeyExchangeMode.values())
+                .filter(item -> item.value == mode)
+                .findFirst();
     }
 
     @Override
