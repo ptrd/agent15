408989988c41d56dd0d15d86f62dce497a1070dd
Peter Doornbosch <peter.doornbosch@luminis.eu>
2024-05-31 17:00:19 +0200

    created java module

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module tech.kwik.agent15 {
+
+    exports net.luminis.tls;
+    exports net.luminis.tls.alert;
+    exports net.luminis.tls.engine;
+    exports net.luminis.tls.env;
+    exports net.luminis.tls.extension;
+    exports net.luminis.tls.handshake;
+    exports net.luminis.tls.util;
+
+    requires at.favre.lib.hkdf;
+}
